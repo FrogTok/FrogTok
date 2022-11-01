5dc3665948cf1129d2c5c0bb98ac8d676959bc38
FrogTok <a0624z@naver.com>
2022-11-01 17:33:13 +0900

    이진 탐색 함수 예제

---
+++ exam/exam7.py
@@ -0,0 +1,16 @@
+from bisect import bisect_left, bisect_right
+
+a = [1, 2, 4, 4, 8]
+x = 4
+
+print(bisect_left(a, x))
+print(bisect_right(a, x))
+
+def count_by_range(a, left_value, right_value):
+    left_index = bisect_left(a, left_value)
+    right_index = bisect_right(a, right_value)
+
+    return right_index - left_index
+
+print(count_by_range(a, 4, 4))
+print(count_by_range(a, -1, 7))
