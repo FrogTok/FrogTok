d52acbf1270e4eb9611f522c6ec4f62cdba72e35
FrogTok <a0624z@naver.com>
2022-11-03 19:32:56 +0900

    문자 비교해서 평가별로 문자열 출력하기

---
+++ codeup100/6069.py
@@ -0,0 +1,12 @@
+c = input()
+
+if('A' == c):
+    print('best!!!')
+elif('B'== c):
+    print('good!!')
+elif('C'== c):
+    print('run!')
+elif('D'== c):
+    print('slowly~')
+else:
+    print('what?')
