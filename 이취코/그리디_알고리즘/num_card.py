41270d889e9542f6d821efcd7bbdb5e0c97046f3
FrogTok <a0624z@naver.com>
2022-11-05 12:27:00 +0900

    숫자 카드 게임

--- 이취코/그리디_알고리즘/num_card.py
+++ 이취코/그리디_알고리즘/num_card.py
@@ -1,11 +1,19 @@
 n, m = map(int, input().split())
-data = [list(map(int, input().split())) for _ in range(n)]
+result = 0
 
-max = 0
+for i in range(n):
+   data = list(map(int, input().split()))
+   min_value = min(data)
+   result = max(result, min_value)
 
-for i in range(len(data)):
-    data[i].sort()
-    if (data[i][0] > max):
-        max = data[i][0]
+# 처음 작성한 코드
+#data = [list(map(int, input().split())) for _ in range(n)]
 
-print(max)
+#result = 0
+
+#for i in range(n):
+#    data[i].sort()
+#    if (data[i][0] > result):
+#        result = data[i][0]
+
+print(result)
