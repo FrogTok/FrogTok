b182122a6d728a74596f65806c6f037dff94391f
FrogTok <a0624z@naver.com>
2022-11-15 11:44:43 +0900

    서로소 집합 알고리즘 예제

---
+++ 이취코/그래프_이론/union.py
@@ -0,0 +1,38 @@
+# 10-1 기본적인 서로소 집합 알고리즘
+# 입력 예시
+# 6 4
+# 1 4
+# 2 3
+# 2 4
+# 5 6
+# 출력 예시
+# 각 원소가 속한 집합: 1 1 1 1 5 5
+# 부모 테이블: 1 1 2 1 5 5
+
+n, m = map(int, input().split())
+
+# i의 부모노드 값 저장
+parents = [ i for i in range(n + 1)]
+
+def find_root(parent, node):
+    while parent != node:
+        node = parent
+        parent = parents[parent]
+    return node
+
+for _ in range(m):
+    n1, n2 = map(int, input().split())
+    root_n1 = find_root(parents[n1], n1)
+    root_n2 = find_root(parents[n2], n2)
+
+    if (root_n1 < root_n2):
+        parents[root_n2] =root_n1
+    else:
+        parents[root_n1] = root_n2
+
+print("각 원소가 속한 집합:", end = ' ')
+for node in range(1, n + 1):
+    print(find_root(parents[node], node), end = ' ')
+print("\n부모 테이블:", end = ' ')
+for node in parents[1:]:
+    print(node, end = ' ')
