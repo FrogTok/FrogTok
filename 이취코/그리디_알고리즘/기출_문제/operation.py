9b1a9966b4b5d535c88d16e7ac897f51096a5284
FrogTok <a0624z@naver.com>
2022-11-18 10:52:49 +0900

    곱하기 혹은 더하기

---
+++ 이취코/그리디_알고리즘/기출_문제/operation.py
@@ -0,0 +1,11 @@
+num = input()
+
+result = int(num[0])
+for i in range(1, len(num)):
+    x = int(num[i])
+    if (x <= 1 or result <= 1):
+        result += x
+    else:
+        result *= x
+
+print(result)
