891b900db52a3e46ba7acddd5faf7fbd4a140daa
FrogTok <a0624z@naver.com>
2022-11-24 10:51:25 +0900

    럭키 스트레이트

---
+++ 이취코/구현/기출_문제/lucky_straight.py
@@ -0,0 +1,13 @@
+n = input()
+
+mid = int(len(n) / 2)
+sum_left = 0
+sum_right = 0
+for i in range(mid):
+    sum_left += int(n[i])
+    sum_right += int(n[i + mid])
+    
+if(sum_left == sum_right):
+    print("LUCKY")
+else:
+    print("READY")
